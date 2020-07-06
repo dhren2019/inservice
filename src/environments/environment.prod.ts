e4931970ad89976dbf9c80a18ab617cd46cecbaa
[Rodrigo] <[roglobar10@gmail.com]>
2020-07-07 00:38:22 +0200

    Enviromments firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,20 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDanuo3x_DyQM9lzHUFzchVG-sbOFbHnS0",
+    authDomain: "flayapp.firebaseapp.com",
+    databaseURL: "https://flayapp.firebaseio.com",
+    projectId: "flayapp",
+    storageBucket: "flayapp.appspot.com",
+    messagingSenderId: "782742914826",
+    appId: "1:782742914826:web:96a4af295ebc5628e29cf3"
+  },
+  onesignal: {
+    appId: '',
+    googleProjectNumber: '',
+    restKey: ''
+  },
+  stripe: {
+    sk: ''
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,27 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDanuo3x_DyQM9lzHUFzchVG-sbOFbHnS0",
+    authDomain: "flayapp.firebaseapp.com",
+    databaseURL: "https://flayapp.firebaseio.com",
+    projectId: "flayapp",
+    storageBucket: "flayapp.appspot.com",
+    messagingSenderId: "782742914826",
+    appId: "1:782742914826:web:96a4af295ebc5628e29cf3"
+  },
+  onesignal: {
+    appId: '',
+    googleProjectNumber: '',
+    restKey: ''
+  },
+  stripe: {
+    sk: ''
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
